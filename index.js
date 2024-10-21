91c0a1bb4a5c9f5d0f8beafcaec2b6bb76997ac1
Mayara Martins <108433575+mayaranm@users.noreply.github.com>
2024-10-21 15:27:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const calculateTip = (event) => {
   event.preventDefault();
   console.log("TESTE");
+  const formData = new FormData(event.target)
+  // pegar value 
+  const value = formData.get("value");
+  //pegar percentage
+  const percentage = formData.get("percentag");
+  console.log(value, percentage);
 };
+
+
