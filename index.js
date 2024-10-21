974d81c902436c4e3badcf7a9dbd43d3f618e01c
Mayara Martins <108433575+mayaranm@users.noreply.github.com>
2024-10-21 15:48:15 -0300

    Update index.js
    montando o JS

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
+const calculatePercentage = (valeu,percentage) => {
+  const percetageValeu = (value/100) * percentage;
+  return percentageValue;
+}
+
+
+
 const calculateTip = (event) => {
+  const tagTotalValue = document.getElementById("totalvalue");
+  const tagTipValue = document.getElementById("tipvalue");
+  const tagOriginalValue = document.getElementById("originalvalue");
   event.preventDefault();
   console.log("TESTE");
-  const formData = new FormData(event.target)
-  // pegar value 
+  const formData = new FormData(event.target) 
   const value = formData.get("value");
-  //pegar percentage
   const percentage = formData.get("percentage");
+  const percentageValeu = calculatePercentage(value, percentage);
+
+  tagOriginalValue.textContent = value;
+  tagTipValue.textContent = percentageValue;
+  tagTotalValue.textContent = Number(value) + percentageValue;
+  
   console.log(value, percentage);
 };
 
+/*                <h1 id="totalvalue" class="align-center mt-2">200R$</h1>
+                <div class="flex gap-2 mt-4">
+                    <div>
+                        <p>Valor da gorjeta</p>
+                        <p id="tipvalue" class="mt-1">100 R$</p>
+                    </div>
 
+                    <div>
+                        <p>Valor Original</p>
+                        <p id="originalvalue" class="mt-1">100 R$</p>
+                    </div>
+*/
